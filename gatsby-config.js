a46d61e6a3d7a25cca26230d7ca434a40ffe9d7f
Yun-sooyong <76515162+Yun-sooyong@users.noreply.github.com>
2023-05-09 03:25:17 +0900

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,7 @@ module.exports = {
       resolve: `gatsby-theme-garden`,
       options: {
         contentPath: `${__dirname}/src/site/notes`,
-        rootNote: `/home`,
+        rootNote: `/index`,
       },
     },
     `gatsby-plugin-netlify`
